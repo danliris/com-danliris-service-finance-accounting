0c24651265a6f55576413080f8c54c6c2353c6f3
Ali Reza <ali.syariati@moonlay.com>
2018-11-08 14:47:18 +0700

    add currency for creditor account

---
+++ Com.Danliris.Service.Finance.Accounting.Lib/Migrations/20181108071150_AddCurrencyInCreditorAccount.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Com.Danliris.Service.Finance.Accounting.Lib.Migrations
+{
+    public partial class AddCurrencyInCreditorAccount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CurrencyCode",
+                table: "CreditorAccounts",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CurrencyCode",
+                table: "CreditorAccounts");
+        }
+    }
+}
