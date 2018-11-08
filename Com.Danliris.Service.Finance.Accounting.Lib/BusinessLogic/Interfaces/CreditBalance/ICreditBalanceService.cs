2e928e92d70145c6ab79b2061c120b0a7e863d4e
Ali Reza <ali.syariati@moonlay.com>
2018-11-08 19:59:58 +0700

    add credit balance

---
+++ Com.Danliris.Service.Finance.Accounting.Lib/BusinessLogic/Interfaces/CreditBalance/ICreditBalanceService.cs
@@ -0,0 +1,15 @@
+﻿using Com.Danliris.Service.Finance.Accounting.Lib.Utilities;
+using Com.Danliris.Service.Finance.Accounting.Lib.ViewModels.CreditBalance;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Com.Danliris.Service.Finance.Accounting.Lib.BusinessLogic.Interfaces.CreditBalance
+{
+    public interface ICreditBalanceService
+    {
+        ReadResponse<CreditBalanceViewModel> GetReport(int page, int size, string suplierName, int month, int year, int offSet);
+        MemoryStream GenerateExcel(string suplierName, int month, int year, int offSet);
+    }
+}
