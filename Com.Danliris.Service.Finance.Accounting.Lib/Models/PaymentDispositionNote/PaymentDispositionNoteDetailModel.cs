1dd0c9035fd3a3c4386a4c46c1a8ee9ffd930cec
achensetyawati <achenzetya@gmail.com>
2019-01-30 15:16:59 +0700

    paymentDispositionNote models

---
+++ Com.Danliris.Service.Finance.Accounting.Lib/Models/PaymentDispositionNote/PaymentDispositionNoteDetailModel.cs
@@ -0,0 +1,35 @@
+﻿using Com.Moonlay.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Com.Danliris.Service.Finance.Accounting.Lib.Models.PaymentDispositionNote
+{
+    public class PaymentDispositionNoteDetailModel : StandardEntity
+    {
+        public virtual int PaymentDispositionNoteItemId { get; set; }
+        [ForeignKey("PaymentDispositionNoteItemId")]
+        public virtual PaymentDispositionNoteModel PaymentDispositionNoteItem { get; set; }
+       
+        public double Price { get; set; }
+        [MaxLength(255)]
+        public string ProductCode { get; set; }
+        public int ProductId { get; set; }
+        [MaxLength(255)]
+        public string ProductName { get; set; }
+        public double Quantity { get; set; }
+        [MaxLength(255)]
+        public string UnitCode { get; set; }
+        public int UnitId { get; set; }
+        [MaxLength(255)]
+        public string UnitName { get; set; }
+        public int PurchasingDispositionExpeditionItemId { get; set; }
+        public int PurchasingDispositionDetailId { get; set; }
+        public int UomId { get; set; }
+        [MaxLength(255)]
+        public string UomUnit { get; set; }
+
+    }
+}
---
+++ Com.Danliris.Service.Finance.Accounting.Lib/Models/PaymentDispositionNote/PaymentDispositionNoteModel.cs
@@ -0,0 +1,44 @@
+﻿using Com.Moonlay.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Com.Danliris.Service.Finance.Accounting.Lib.Models.PaymentDispositionNote
+{
+    public class PaymentDispositionNoteModel : StandardEntity, IValidatableObject
+    {
+        [MaxLength(255)]
+        public string BGCheckNumber { get; set; }
+        [MaxLength(1000)]
+        public string BankAccountName { get; set; }
+        [MaxLength(255)]
+        public string BankAccountNumber { get; set; }
+        [MaxLength(255)]
+        public string BankCode { get; set; }
+        public int BankId { get; set; }
+        [MaxLength(1000)]
+        public string BankName { get; set; }
+        [MaxLength(255)]
+        public string BankCurrencyCode { get; set; }
+        public int BankCurrencyId { get; set; }
+        public double BankCurrencyRate { get; set; }
+        [MaxLength(255)]
+        public string PaymentDispositionNo { get; set; }
+        public double Amount { get; set; }
+        [MaxLength(255)]
+        public string SupplierCode { get; set; }
+        public int SupplierId { get; set; }
+        public bool SupplierImport { get; set; }
+        [MaxLength(1000)]
+        public string SupplierName { get; set; }
+        public DateTimeOffset PaymentDate { get; set; }
+        public string BankAccountCOA { get; set; }
+        public virtual ICollection<PaymentDispositionNoteItemModel> Items { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
