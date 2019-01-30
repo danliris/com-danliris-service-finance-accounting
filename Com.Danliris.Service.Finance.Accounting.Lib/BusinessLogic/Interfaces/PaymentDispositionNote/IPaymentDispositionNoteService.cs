9296824d396b3265f2339c45bfa07ab78e85784d
achensetyawati <achenzetya@gmail.com>
2019-01-30 15:18:07 +0700

    PaymentDispositionNote service

---
+++ Com.Danliris.Service.Finance.Accounting.Lib/BusinessLogic/Interfaces/PaymentDispositionNote/IPaymentDispositionNoteService.cs
@@ -0,0 +1,13 @@
+﻿using Com.Danliris.Service.Finance.Accounting.Lib.Models.PaymentDispositionNote;
+using Com.Danliris.Service.Finance.Accounting.Lib.Utilities.BaseInterface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Danliris.Service.Finance.Accounting.Lib.BusinessLogic.Interfaces.PaymentDispositionNote
+{
+    public interface IPaymentDispositionNoteService : IBaseService<PaymentDispositionNoteModel>
+    {
+
+    }
+}
