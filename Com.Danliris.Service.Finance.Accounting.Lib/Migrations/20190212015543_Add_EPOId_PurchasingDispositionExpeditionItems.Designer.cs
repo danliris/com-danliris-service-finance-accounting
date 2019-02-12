17429facf3636d33e25e3d6cb5b472dd411f5f1b
achensetyawati <achenzetya@gmail.com>
2019-02-12 11:09:37 +0700

    Disposition Migrations

--- Com.Danliris.Service.Finance.Accounting.Lib/Migrations/20190212015543_Add_EPOId_PurchasingDispositionExpeditionItems.Designer.cs
+++ Com.Danliris.Service.Finance.Accounting.Lib/Migrations/20190212015543_Add_EPOId_PurchasingDispositionExpeditionItems.Designer.cs
@@ -4,14 +4,16 @@ using Com.Danliris.Service.Finance.Accounting.Lib;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Com.Danliris.Service.Finance.Accounting.Lib.Migrations
 {
     [DbContext(typeof(FinanceDbContext))]
-    partial class FinanceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190212015543_Add_EPOId_PurchasingDispositionExpeditionItems")]
+    partial class Add_EPOId_PurchasingDispositionExpeditionItems
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -489,8 +491,6 @@ namespace Com.Danliris.Service.Finance.Accounting.Lib.Migrations
                         .HasMaxLength(255);
 
                     b.Property<DateTime>("DeletedUtc");
-
-                    b.Property<string>("EPOId");
 
                     b.Property<bool>("IsDeleted");
 
