17d77741da6eb39c926f5c1005008982e37f6b6b
adepamungkas <ade.pamungkas9@gmail.com>
2020-03-19 14:08:51 +0700

    API wrapper test

---
+++ Com.Danliris.Service.Finance.Accounting.Test/Utilities/APIEndpointTest.cs
@@ -0,0 +1,20 @@
+﻿using Com.Danliris.Service.Finance.Accounting.Lib.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Com.Danliris.Service.Finance.Accounting.Test.Utilities
+{
+   public class APIEndpointTest
+    {
+
+
+        [Fact]
+        public void Should_succes_intantiate_APIEndpoint()
+        {
+            var result = new APIEndpoint();
+            Assert.NotNull(result);
+        }
+    }
+}
---
+++ Com.Danliris.Service.Finance.Accounting.Test/Utilities/APIWrapperTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Com.Danliris.Service.Finance.Accounting.Lib.Utilities;
+using Moq;
+using Xunit;
+
+namespace Com.Danliris.Service.Finance.Accounting.Test.Utilities
+{
+   public class APIWrapperTest
+    {
+        [Fact]
+        public void Should_succes_intantiate_APIWrapper()
+        {
+            var result =new APIWrapper()
+            {
+                apiVersion ="1"
+            };
+            Assert.NotNull(result.apiVersion);
+        }
+        
+    }
+}
