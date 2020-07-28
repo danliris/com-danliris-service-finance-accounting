7b3a2f91d339b2bbc328bcb738d5230224e143a1
Ahmad Mukhtar <amx23baradja@gmail.com>
2020-07-28 22:08:36 +0700

    Create NumbertoTextENTest.cs

---
+++ Com.Danliris.Service.Finance.Accounting.Test/Utilities/NumbertoTextENTest.cs
@@ -0,0 +1,36 @@
+﻿using Com.Danliris.Service.Sales.Lib.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Com.Danliris.Service.Finance.Accounting.Test.Utilities
+{
+    public class NumbertoTextENTest
+    {
+        [Fact]
+        public void toWords_With_DozenNumber_Return_Success()
+        {
+            var result = NumberToTextEN.toWords(16);
+            Assert.NotEmpty(result);
+            Assert.Equal("Sixteen  ", result);
+        }
+
+        [Fact]
+        public void toWords_withHundredNumber_Return_Success()
+        {
+            var result = NumberToTextEN.toWords(200);
+            Assert.NotEmpty(result);
+            Assert.Equal("Two  Hundred  ", result);
+
+        }
+
+        [Fact]
+        public void toWords_withTwentiesNumber_Return_Success()
+        {
+            var result = NumberToTextEN.toWords(21);
+            Assert.NotEmpty(result);
+            Assert.Equal("Twenty One  ", result);
+        }
+    }
+}
