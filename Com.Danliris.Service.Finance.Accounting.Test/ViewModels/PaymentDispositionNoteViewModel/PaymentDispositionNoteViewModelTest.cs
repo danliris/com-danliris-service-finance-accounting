59381b15274fda619018837d295acd8c4d9edbac
adepamungkas <ade.pamungkas9@gmail.com>
2020-08-04 10:08:53 +0700

    Add Unit Tes ViwModel

---
+++ Com.Danliris.Service.Finance.Accounting.Test/ViewModels/PaymentDispositionNoteViewModel/PaymentDispositionNoteViewModelTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Com.Danliris.Service.Finance.Accounting.Test.ViewModels.PaymentDispositionNoteViewModel
+{
+   public class PaymentDispositionNoteViewModelTest
+    {
+        //[Fact]
+        //public void validate_Currency()
+        //{
+        //    PaymentDispositionNoteViewModel viewModel = new PaymentDispositionNoteViewModel();
+        //    var result = viewModel.Validate(null);
+        //    Assert.True(0 < result.Count());
+        //}
+    }
+}
