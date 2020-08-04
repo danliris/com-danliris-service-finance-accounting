1bf7d683189daefcc3d67808b5a8d2a1bdfa4ae8
adepamungkas <ade.pamungkas9@gmail.com>
2020-08-04 10:47:29 +0700

    PaymentDispositionNoteViewModelTest

--- Com.Danliris.Service.Finance.Accounting.Test/ViewModels/PaymentDispositionNoteViewModel/PaymentDispositionNoteViewModelTest.cs
+++ Com.Danliris.Service.Finance.Accounting.Test/ViewModels/PaymentDispositionNoteViewModel/PaymentDispositionNoteViewModelTest.cs
@@ -2,17 +2,30 @@
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
+using Com.Danliris.Service.Finance.Accounting.Lib.ViewModels.PaymentDispositionNoteViewModel;
+using System.Linq;
 
 namespace Com.Danliris.Service.Finance.Accounting.Test.ViewModels.PaymentDispositionNoteViewModel
 {
    public class PaymentDispositionNoteViewModelTest
     {
-        //[Fact]
-        //public void validate_Currency()
-        //{
-        //    PaymentDispositionNoteViewModel viewModel = new PaymentDispositionNoteViewModel();
-        //    var result = viewModel.Validate(null);
-        //    Assert.True(0 < result.Count());
-        //}
+        [Fact]
+        public void validate_Currency()
+        {
+            var viewModel = new Com.Danliris.Service.Finance.Accounting.Lib.ViewModels.PaymentDispositionNoteViewModel.PaymentDispositionNoteViewModel()
+            {
+                AccountBank =new Lib.ViewModels.NewIntegrationViewModel.AccountBankViewModel()
+                {
+                    Id=1,
+                    AccountName="ade",
+                    Currency=new Lib.ViewModels.NewIntegrationViewModel.CurrencyViewModel()
+                    {
+                        Code = "IDR"
+                    }
+                }
+            };
+            var result = viewModel.Validate(null);
+            Assert.True(0 < result.Count());
+        }
     }
 }
