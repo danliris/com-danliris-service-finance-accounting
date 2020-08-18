f7a44badadbff600a0f10dde1a8f04aca8a4be9d
adepamungkas <ade.pamungkas9@gmail.com>
2020-08-18 14:33:26 +0700

    unit test viewModels

---
+++ Com.Danliris.Service.Finance.Accounting.Test/ViewModels/JournalTransaction/JournalTransactionReportHeaderViewModelTest.cs
@@ -0,0 +1,29 @@
+﻿using Com.Danliris.Service.Finance.Accounting.Lib.ViewModels.JournalTransaction;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Com.Danliris.Service.Finance.Accounting.Test.ViewModels.JournalTransaction
+{
+   public class JournalTransactionReportHeaderViewModelTest
+    {
+        [Fact]
+        public void should_success_intantiate()
+        {
+            JournalTransactionReportHeaderViewModel viewModel = new JournalTransactionReportHeaderViewModel()
+            {
+                ReferenceNo = "ReferenceNo",
+                Description = "Description",
+                Items =new List<JournalTransactionReportViewModel>()
+                {
+                    new JournalTransactionReportViewModel()
+                }
+            };
+
+            Assert.NotNull(viewModel.Items);
+            Assert.Equal("ReferenceNo", viewModel.ReferenceNo);
+            Assert.Equal("Description", viewModel.Description);
+        }
+    }
+}
---
+++ Com.Danliris.Service.Finance.Accounting.Test/ViewModels/PurchasingDispositionReport/PurchasingDispositionReportPostedViewModelTest.cs
@@ -0,0 +1,29 @@
+﻿using Com.Danliris.Service.Finance.Accounting.Lib.ViewModels.IntegrationViewModel;
+using Com.Danliris.Service.Finance.Accounting.Lib.ViewModels.PurchasingDispositionReport;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Com.Danliris.Service.Finance.Accounting.Test.ViewModels.PurchasingDispositionReport
+{
+  public  class PurchasingDispositionReportPostedViewModelTest
+    {
+        public void should_success_intantiate()
+        {
+            PurchasingDispositionReportPostedViewModel viewModel = new PurchasingDispositionReportPostedViewModel()
+            {
+                Data =new List<PurchasingDispositionViewModel>()
+                {
+                    new PurchasingDispositionViewModel()
+                },
+                DateFrom =DateTimeOffset.MinValue,
+                DateTo=DateTimeOffset.MaxValue
+            };
+
+            Assert.NotNull(viewModel.Data);
+            Assert.Equal(DateTimeOffset.MinValue, viewModel.DateFrom);
+            Assert.Equal(DateTimeOffset.MaxValue, viewModel.DateTo);
+        }
+    }
+}
