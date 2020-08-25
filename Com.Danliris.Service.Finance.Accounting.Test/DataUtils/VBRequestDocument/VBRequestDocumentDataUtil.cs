ff255ea74858d206c0eba0844494104b8774f457
adepamungkas <ade.pamungkas9@gmail.com>
2020-08-25 16:36:22 +0700

    Create VBRequestDocumentServiceTest

---
+++ Com.Danliris.Service.Finance.Accounting.Test/DataUtils/VBRequestDocument/VBRequestDocumentDataUtil.cs
@@ -0,0 +1,201 @@
+﻿using Com.Danliris.Service.Finance.Accounting.Lib;
+using Com.Danliris.Service.Finance.Accounting.Lib.BusinessLogic.VBRequestDocument;
+using Com.Danliris.Service.Finance.Accounting.Lib.Models.VBRequestDocument;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Danliris.Service.Finance.Accounting.Test.DataUtils.VBRequestDocument
+{
+    public class VBRequestDocumentDataUtil
+    {
+        private readonly VBRequestDocumentService _service;
+        private FinanceDbContext _dbContext;
+        public VBRequestDocumentDataUtil(VBRequestDocumentService service, FinanceDbContext dbContext)
+        {
+            _service = service;
+            _dbContext = dbContext;
+        }
+
+       
+
+        public VBRequestDocumentModel GetTestData_VBRequestDocument()
+        {
+
+            var testData = GetNewData_VBRequestDocument();
+            _dbContext.VBRequestDocuments.Add(testData);
+            _dbContext.SaveChanges();
+            return testData;
+        }
+
+        public VBRequestDocumentModel GetNewData_VBRequestDocument()
+        {
+            VBRequestDocumentModel TestData = new VBRequestDocumentModel("documentNo", DateTimeOffset.Now, DateTimeOffset.Now, 1, "suppliantUnitCode", "suppliantUnitName", 1, "suppliantDivisionCode", "suppliantDivisionName", 1, "IDR", "Rp", "currencyDescription", 1, "Purpose", 1, true, true, true, new VBType());
+
+            return TestData;
+        }
+
+
+        public VBRequestDocumentItemModel GetNewData_VBRequestDocumentItemModel()
+        {
+            var vBRequestDocument = GetTestData_VBRequestDocument();
+            VBRequestDocumentItemModel TestData = new VBRequestDocumentItemModel(vBRequestDocument.Id,1,"unitName","unitCode",1,"divisionName","divisionCode",1,"epoNo",true,1,"incomeTaxName",1,"incomeTaxBy",1,true,1);
+
+            return TestData;
+        }
+
+        public VBRequestDocumentItemModel GetTestData_VBRequestDocumentItem()
+        {
+
+            var testData = GetNewData_VBRequestDocumentItemModel();
+            _dbContext.VBRequestDocumentItems.Add(testData);
+            _dbContext.SaveChanges();
+            return testData;
+        }
+
+        public VBRequestDocumentNonPOFormDto GetNewData_VBRequestDocumentNonPOFormDto()
+        {
+            VBRequestDocumentNonPOFormDto form = new VBRequestDocumentNonPOFormDto()
+            {
+                Amount = 1,
+                Currency = new CurrencyDto()
+                {
+                    Code = "IDR",
+                    Description = "Description",
+                    Rate = 1,
+                    Symbol = "Rp"
+                },
+                Date = DateTimeOffset.Now,
+                Purpose = "Purpose",
+                RealizationEstimationDate = DateTimeOffset.Now,
+                SuppliantUnit = new UnitDto()
+                {
+                    Code = "Code",
+                    Division = new DivisionDto()
+                    {
+                        Code = "Code",
+                        Name = "GARMENT"
+                    },
+                    Name = "Meter",
+                    VBDocumentLayoutOrder = 1
+                },
+                Items = new List<VBRequestDocumentNonPOItemFormDto>()
+                {
+                    new VBRequestDocumentNonPOItemFormDto()
+                    {
+                        IsSelected=true,
+                        Unit=new UnitDto()
+                        {
+                            Code="Code",
+                            Division = new DivisionDto()
+                            {
+                                Code = "Code",
+                                Name = "Name",
+                                
+                            },
+                            Name = "Name",
+                            VBDocumentLayoutOrder = 1
+                        }
+                    }
+                }
+            };
+            return form;
+        }
+
+        public VBRequestDocumentWithPOFormDto GetNewData_VBRequestDocumentWithPOFormDto()
+        {
+            return new VBRequestDocumentWithPOFormDto()
+            {
+                Amount = 1,
+                Currency = new CurrencyDto()
+                {
+                    Code = "IDR",
+                    Description = "Description",
+                    Rate = 1,
+                    Symbol = "Rp"
+                },
+                Date = DateTimeOffset.Now,
+                Purpose = "Purpose",
+                RealizationEstimationDate = DateTimeOffset.Now,
+                SuppliantUnit = new UnitDto()
+                {
+                    Code = "Code",
+                    Division = new DivisionDto()
+                    {
+                        Code = "Code",
+                        Name = "GARMENT"
+                    },
+                    Name = "Meter",
+                    VBDocumentLayoutOrder = 1
+                },
+                
+                Items = new List<VBRequestDocumentWithPOItemFormDto>()
+                {
+                    new VBRequestDocumentWithPOItemFormDto()
+                    {
+                       
+                       PurchaseOrderExternal=new PurchaseOrderExternal()
+                       {
+                           incomeTax=new IncomeTaxDto()
+                           {
+                               name="name",
+                               rate=1
+                           },
+                           
+                           incomeTaxBy="incomeTaxBy",
+                           no="1",
+                           unit=new OldUnitDto()
+                           {
+                               
+                               code="code",
+                               division =new OldDivisionDto()
+                               {
+                                   code="code",
+                                   name="name"
+                               },
+                               name="name"
+                           },
+                           useIncomeTax=true,
+                           useVat=true,
+                           Details=new List<PurchaseOrderExternalItem>()
+                           {
+                               new PurchaseOrderExternalItem()
+                               {
+                                   
+                                   Conversion=1,
+                                   DealQuantity=1,
+                                   DealUOM=new UnitOfMeasurement()
+                                   {
+                                       unit="meter" 
+                                   },
+                                   DefaultQuantity=1,
+                                   Price=1,
+                                   Product=new Product()
+                                   {
+                                       name="ProdukName",
+                                       uom=new UnitOfMeasurement()
+                                       {
+                                           unit="meter"
+                                       },
+                                       code="code"
+                                   },
+                                   UseVat=true
+                               }
+                           },
+                           
+                       }
+                    }
+                }
+            };
+        }
+      //  VBRequestDocumentItems
+
+        //public async Task<VBRequestDocumentModel> GetTestData()
+        //{
+        //    VBRequestDocumentModel model = GetNewData();
+        //    await Service.CreateNonPO()
+        //    return await Service.ReadByIdAsync(model.Id);
+        //}
+    }
+}
---
+++ Com.Danliris.Service.Finance.Accounting.Test/Services/VBRequestDocument/VBRequestDocumentServiceTest.cs
@@ -0,0 +1,220 @@
+﻿using Com.Danliris.Service.Finance.Accounting.Lib;
+using Com.Danliris.Service.Finance.Accounting.Lib.BusinessLogic.VBRequestDocument;
+using Com.Danliris.Service.Finance.Accounting.Lib.Models.VBRequestDocument;
+using Com.Danliris.Service.Finance.Accounting.Lib.Services.HttpClientService;
+using Com.Danliris.Service.Finance.Accounting.Lib.Services.IdentityService;
+using Com.Danliris.Service.Finance.Accounting.Test.DataUtils.VBRequestDocument;
+using Com.Danliris.Service.Finance.Accounting.Test.Helpers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Com.Danliris.Service.Finance.Accounting.Test.Services.VBRequestDocument
+{
+  public  class VBRequestDocumentServiceTest
+    {
+        private const string ENTITY = "VBRequestDocuments";
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        protected string GetCurrentMethod()
+        {
+
+            StackTrace st = new StackTrace();
+            StackFrame sf = st.GetFrame(1);
+
+            return string.Concat(sf.GetMethod().Name, "_", ENTITY);
+        }
+
+        protected string GetCurrentAsyncMethod([CallerMemberName] string methodName = "")
+        {
+            var method = new StackTrace()
+                .GetFrames()
+                .Select(frame => frame.GetMethod())
+                .FirstOrDefault(item => item.Name == methodName);
+
+            return method.Name;
+
+        }
+        private FinanceDbContext _dbContext(string testName)
+        {
+            var serviceProvider = new ServiceCollection()
+              .AddEntityFrameworkInMemoryDatabase()
+              .BuildServiceProvider();
+
+            DbContextOptionsBuilder<FinanceDbContext> optionsBuilder = new DbContextOptionsBuilder<FinanceDbContext>();
+            optionsBuilder
+                .UseInMemoryDatabase(testName)
+                .ConfigureWarnings(w => w.Ignore(InMemoryEventId.TransactionIgnoredWarning))
+                 .UseInternalServiceProvider(serviceProvider);
+
+            FinanceDbContext dbContext = new FinanceDbContext(optionsBuilder.Options);
+
+            return dbContext;
+        }
+
+        private Mock<IServiceProvider> GetServiceProvider()
+        {
+            var serviceProvider = new Mock<IServiceProvider>();
+
+            serviceProvider
+                .Setup(x => x.GetService(typeof(IHttpClientService)))
+                .Returns(new HttpClientTestService());
+
+            serviceProvider
+                .Setup(x => x.GetService(typeof(IIdentityService)))
+                .Returns(new IdentityService() { Token = "Token", Username = "Test", TimezoneOffset = 7 });
+
+
+            return serviceProvider;
+        }
+
+        private VBRequestDocumentDataUtil _dataUtil(VBRequestDocumentService service, FinanceDbContext financeDbContext)
+        {
+            return new VBRequestDocumentDataUtil(service, financeDbContext);
+        }
+
+        [Fact]
+        public async Task CreateNonPO_Return_Success()
+        {
+            FinanceDbContext dbContext = _dbContext(GetCurrentAsyncMethod());
+
+            VBRequestDocumentService service = new VBRequestDocumentService(dbContext, GetServiceProvider().Object);
+            VBRequestDocumentNonPOFormDto form = _dataUtil(service, dbContext).GetNewData_VBRequestDocumentNonPOFormDto();
+            VBRequestDocumentModel data = _dataUtil(service, dbContext).GetTestData_VBRequestDocument();
+
+            int result = await service.CreateNonPO(form);
+            Assert.NotEqual(0, result);
+        }
+
+        [Fact]
+        public async Task CreateNonPO_Throw_Exception()
+        {
+            FinanceDbContext dbContext = _dbContext(GetCurrentAsyncMethod());
+
+            VBRequestDocumentService service = new VBRequestDocumentService(dbContext, GetServiceProvider().Object);
+            VBRequestDocumentNonPOFormDto form = _dataUtil(service, dbContext).GetNewData_VBRequestDocumentNonPOFormDto();
+            form.Items = null;
+
+            await Assert.ThrowsAsync<System.ArgumentNullException>(()=>service.CreateNonPO(form));
+        }
+
+
+        [Fact]
+        public void CreateWithPO_Return_Success()
+        {
+            FinanceDbContext dbContext = _dbContext(GetCurrentAsyncMethod());
+
+            VBRequestDocumentService service = new VBRequestDocumentService(dbContext, GetServiceProvider().Object);
+            VBRequestDocumentWithPOFormDto form = _dataUtil(service, dbContext).GetNewData_VBRequestDocumentWithPOFormDto();
+            VBRequestDocumentModel data = _dataUtil(service, dbContext).GetTestData_VBRequestDocument();
+
+            int result =  service.CreateWithPO(form);
+            Assert.NotEqual(0, result);
+        }
+
+        [Fact]
+        public async Task DeleteNonPO_Return_Success()
+        {
+            FinanceDbContext dbContext = _dbContext(GetCurrentAsyncMethod());
+
+            VBRequestDocumentService service = new VBRequestDocumentService(dbContext, GetServiceProvider().Object);
+            VBRequestDocumentItemModel data = _dataUtil(service, dbContext).GetTestData_VBRequestDocumentItem();
+
+            int result = await service.DeleteNonPO(data.Id);
+            Assert.NotEqual(0, result);
+        }
+
+
+        [Fact]
+        public void DeleteWithPO_Return_Success()
+        {
+            FinanceDbContext dbContext = _dbContext(GetCurrentAsyncMethod());
+
+            VBRequestDocumentService service = new VBRequestDocumentService(dbContext, GetServiceProvider().Object);
+            VBRequestDocumentItemModel data = _dataUtil(service, dbContext).GetTestData_VBRequestDocumentItem();
+
+            int result =  service.DeleteWithPO(data.Id);
+            Assert.NotEqual(0, result);
+        }
+
+
+        [Fact]
+        public void Get_Return_Success()
+        {
+            FinanceDbContext dbContext = _dbContext(GetCurrentAsyncMethod());
+
+            VBRequestDocumentService service = new VBRequestDocumentService(dbContext, GetServiceProvider().Object);
+            VBRequestDocumentItemModel data = _dataUtil(service, dbContext).GetTestData_VBRequestDocumentItem();
+
+            var orderData = new
+            {
+                DocumentNo = "desc"
+            };
+            string order =JsonConvert.SerializeObject(orderData);
+          
+            var result = service.Get(1,1, order,new List<string>(),"","{}");
+            Assert.NotNull( result);
+            Assert.True(0 < result.Data.Count());
+        }
+
+        [Fact]
+        public void GetNonPOById_Return_Success()
+        {
+            FinanceDbContext dbContext = _dbContext(GetCurrentAsyncMethod());
+
+            VBRequestDocumentService service = new VBRequestDocumentService(dbContext, GetServiceProvider().Object);
+            VBRequestDocumentItemModel data = _dataUtil(service, dbContext).GetTestData_VBRequestDocumentItem();
+
+            var result = service.GetNonPOById(data.Id);
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task GetNonPOById_Return_Null()
+        {
+            FinanceDbContext dbContext = _dbContext(GetCurrentAsyncMethod());
+
+            VBRequestDocumentService service = new VBRequestDocumentService(dbContext, GetServiceProvider().Object);
+            VBRequestDocumentItemModel data = _dataUtil(service, dbContext).GetTestData_VBRequestDocumentItem();
+
+            var result =await service.GetNonPOById(data.Id+1);
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void GetWithPOById_Return_Success()
+        {
+            FinanceDbContext dbContext = _dbContext(GetCurrentAsyncMethod());
+
+            VBRequestDocumentService service = new VBRequestDocumentService(dbContext, GetServiceProvider().Object);
+            VBRequestDocumentItemModel data = _dataUtil(service, dbContext).GetTestData_VBRequestDocumentItem();
+
+            var result =  service.GetWithPOById(data.Id );
+            Assert.NotNull(result);
+            Assert.NotEqual(0,result.Id);
+        }
+
+        [Fact]
+        public async Task UpdateNonPO_Return_Null()
+        {
+            FinanceDbContext dbContext = _dbContext(GetCurrentAsyncMethod());
+
+            VBRequestDocumentService service = new VBRequestDocumentService(dbContext, GetServiceProvider().Object);
+            VBRequestDocumentNonPOFormDto form = _dataUtil(service, dbContext).GetNewData_VBRequestDocumentNonPOFormDto();
+            VBRequestDocumentItemModel data = _dataUtil(service, dbContext).GetTestData_VBRequestDocumentItem();
+
+            int result = await service.UpdateNonPO(data.Id, form);
+            Assert.NotEqual(0,result);
+        }
+
+    }
+}
