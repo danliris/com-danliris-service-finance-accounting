2387aea99c5b2e3fec6e4aef33792fbc9dab8b2e
leslieaula <cristiano.ulo@gmail.com>
2020-08-27 16:44:00 +0700

    Fix Read By Id

---
+++ Com.Danliris.Service.Finance.Accounting.Lib/BusinessLogic/VBRealizationDocument/VBRealizationWithPOItemDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Com.Danliris.Service.Finance.Accounting.Lib.BusinessLogic.VBRealizationDocument
+{
+    public class VBRealizationWithPOItemDto
+    {
+        public VBRealizationWithPOItemDto()
+        {
+        }
+
+        public UnitPaymentOrderDto UnitPaymentOrder { get; internal set; }
+        public int Id { get; internal set; }
+    }
+}
