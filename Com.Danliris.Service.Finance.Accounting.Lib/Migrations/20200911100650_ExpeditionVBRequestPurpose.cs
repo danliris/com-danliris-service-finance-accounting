02b5d98e0b13050cf284446241ecf868ba824b99
leslieaula <cristiano.ulo@gmail.com>
2020-09-11 17:07:34 +0700

    Add Expedition

---
+++ Com.Danliris.Service.Finance.Accounting.Lib/Migrations/20200911100650_ExpeditionVBRequestPurpose.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Com.Danliris.Service.Finance.Accounting.Lib.Migrations
+{
+    public partial class ExpeditionVBRequestPurpose : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Purpose",
+                table: "VBRealizationDocumentExpeditions",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Purpose",
+                table: "VBRealizationDocumentExpeditions");
+        }
+    }
+}
