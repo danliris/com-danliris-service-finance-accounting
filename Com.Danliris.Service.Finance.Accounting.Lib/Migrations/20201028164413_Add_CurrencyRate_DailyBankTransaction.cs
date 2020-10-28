db0336ac45e5a48814a51d7818f917722376d230
Arie Nandjar <52964987+arijulianton20@users.noreply.github.com>
2020-10-28 23:46:16 +0700

    [UPDATE] Add CurrencyRate on DailyBankTransaction

---
+++ Com.Danliris.Service.Finance.Accounting.Lib/Migrations/20201028164413_Add_CurrencyRate_DailyBankTransaction.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Com.Danliris.Service.Finance.Accounting.Lib.Migrations
+{
+    public partial class Add_CurrencyRate_DailyBankTransaction : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "CurrencyRate",
+                table: "DailyBankTransactions",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CurrencyRate",
+                table: "DailyBankTransactions");
+        }
+    }
+}
