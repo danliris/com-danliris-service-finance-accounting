284bf2507053aa6e0428f0cab790deab06424582
adepamungkas <ade.pamungkas9@gmail.com>
2021-01-08 10:02:53 +0000

    Create GarmentPurchasingExpeditionServiceTest.cs

---
+++ Com.Danliris.Service.Finance.Accounting.Test/Services/GarmentPurchasingExpedition/GarmentPurchasingExpeditionServiceTest.cs
@@ -0,0 +1,56 @@
+﻿using Com.Danliris.Service.Finance.Accounting.Lib;
+using Com.Danliris.Service.Finance.Accounting.Lib.Services.IdentityService;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Com.Danliris.Service.Finance.Accounting.Test.Services.GarmentPurchasingExpedition
+{
+  public  class GarmentPurchasingExpeditionServiceTest
+    {
+        private const string Entity = "PayrollCorrections";
+
+        private string GetCurrentAsyncMethod([CallerMemberName] string methodName = "")
+        {
+            var method = new StackTrace()
+                .GetFrames()
+                .Select(frame => frame.GetMethod())
+                .FirstOrDefault(item => item.Name == methodName);
+
+            return method.Name;
+
+        }
+
+        private FinanceDbContext GetDbContext(string testName)
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddEntityFrameworkInMemoryDatabase()
+                .BuildServiceProvider();
+
+            var optionsBuilder = new DbContextOptionsBuilder<FinanceDbContext>();
+            optionsBuilder
+                .UseInMemoryDatabase(testName)
+                .ConfigureWarnings(w => w.Ignore(InMemoryEventId.TransactionIgnoredWarning))
+                .UseInternalServiceProvider(serviceProvider);
+
+            return new FinanceDbContext(optionsBuilder.Options);
+        }
+
+        private Mock<IServiceProvider> GetServiceProviderMock()
+        {
+            var serviceProviderMock = new Mock<IServiceProvider>();
+            serviceProviderMock
+                .Setup(serviceProvider => serviceProvider.GetService(typeof(IIdentityService)))
+                .Returns(new IdentityService() { TimezoneOffset = 0, Token = "token", Username = "username" });
+
+            return serviceProviderMock;
+        }
+    }
+}
