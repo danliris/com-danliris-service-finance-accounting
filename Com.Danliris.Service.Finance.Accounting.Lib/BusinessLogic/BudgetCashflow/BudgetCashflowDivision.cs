19653642da8ae4d88af4d0a56c8e3b6ba6e4830d
Leslie Aula <leslie.aula@moonlay.com>
2021-01-18 14:35:49 +0700

    add division header

---
+++ Com.Danliris.Service.Finance.Accounting.Lib/BusinessLogic/BudgetCashflow/BudgetCashflowDivision.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Danliris.Service.Finance.Accounting.Lib.BusinessLogic.BudgetCashflow
+{
+    public class BudgetCashflowDivision
+    {
+        public BudgetCashflowDivision(List<string> headers, List<BudgetCashflowItemDivisionDto> items)
+        {
+            Headers = headers;
+            Items = items;
+        }
+
+        public List<string> Headers { get; private set; }
+        public List<BudgetCashflowItemDivisionDto> Items { get; private set; }
+    }
+}
---
+++ Com.Danliris.Service.Finance.Accounting.Lib/BusinessLogic/BudgetCashflow/CurrencyDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Com.Danliris.Service.Finance.Accounting.Lib.BusinessLogic.BudgetCashflow
+{
+    public class CurrencyDto
+    {
+        public int Id { get; private set; }
+        public string Code { get; private set; }
+        public double Rate { get; private set; }
+    }
+}
