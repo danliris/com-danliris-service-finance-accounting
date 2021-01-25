3f865b9129c96f33133288b09ec307ecb109045c
Leslie Aula <leslie.aula@moonlay.com>
2021-01-25 10:49:17 +0700

    Refactor Get Cashflow Division

--- Com.Danliris.Service.Finance.Accounting.Lib/BusinessLogic/BudgetCashflow/BudgetCashflowDivision.cs
+++ Com.Danliris.Service.Finance.Accounting.Lib/BusinessLogic/BudgetCashflow/BudgetCashflowDivision.cs
@@ -6,13 +6,19 @@ namespace Com.Danliris.Service.Finance.Accounting.Lib.BusinessLogic.BudgetCashfl
 {
     public class BudgetCashflowDivision
     {
-        public BudgetCashflowDivision(List<string> headers, List<BudgetCashflowItemDto> items)
+        public BudgetCashflowDivision()
+        {
+            Headers = new List<string>();
+            Items = new List<BudgetCashflowDivisionItemDto>();
+        }
+
+        public BudgetCashflowDivision(List<string> headers, List<BudgetCashflowDivisionItemDto> items)
         {
             Headers = headers;
             Items = items;
         }
 
         public List<string> Headers { get; private set; }
-        public List<BudgetCashflowItemDto> Items { get; private set; }
+        public List<BudgetCashflowDivisionItemDto> Items { get; private set; }
     }
 }
---
+++ Com.Danliris.Service.Finance.Accounting.Lib/BusinessLogic/BudgetCashflow/BudgetCashflowDivisionUnitItemDto.cs
@@ -0,0 +1,28 @@
+﻿using Com.Danliris.Service.Finance.Accounting.Lib.Models.BudgetCashflow;
+
+namespace Com.Danliris.Service.Finance.Accounting.Lib.BusinessLogic.BudgetCashflow
+{
+    public class BudgetCashflowDivisionUnitItemDto
+    {
+        public BudgetCashflowDivisionUnitItemDto(BudgetCashflowUnitModel cashflowUnit, UnitDto divisionUnit)
+        {
+            CashflowUnit = cashflowUnit;
+            Unit = divisionUnit;
+        }
+
+        public BudgetCashflowDivisionUnitItemDto(DivisionDto division, double divisionCurrencyNominal, double divisionNominal, double divisionActual)
+        {
+            Division = division;
+            DivisionCurrencyNominal = divisionCurrencyNominal;
+            DivisionNominal = divisionNominal;
+            DivisionActual = divisionActual;
+        }
+
+        public BudgetCashflowUnitModel CashflowUnit { get; private set; }
+        public UnitDto Unit { get; private set; }
+        public DivisionDto Division { get; private set; }
+        public double DivisionCurrencyNominal { get; private set; }
+        public double DivisionNominal { get; private set; }
+        public double DivisionActual { get; private set; }
+    }
+}
