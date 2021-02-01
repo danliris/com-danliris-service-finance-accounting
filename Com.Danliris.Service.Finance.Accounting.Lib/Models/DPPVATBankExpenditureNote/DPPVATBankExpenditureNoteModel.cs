1cb32f00c102f0b203bbf1afff93d183c016c7f5
Leslie Aula <leslie.aula@moonlay.com>
2021-02-02 06:43:26 +0700

    Modify Column Size

--- Com.Danliris.Service.Finance.Accounting.Lib/Models/DPPVATBankExpenditureNote/DPPVATBankExpenditureNoteModel.cs
+++ Com.Danliris.Service.Finance.Accounting.Lib/Models/DPPVATBankExpenditureNote/DPPVATBankExpenditureNoteModel.cs
@@ -1,6 +1,7 @@
 ﻿using Com.Moonlay.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Com.Danliris.Service.Finance.Accounting.Lib.Models.DPPVATBankExpenditureNote
@@ -30,17 +31,24 @@ namespace Com.Danliris.Service.Finance.Accounting.Lib.Models.DPPVATBankExpenditu
             Date = date;
         }
 
+        [MaxLength(32)]
         public string DocumentNo { get; private set; }
         public int BankAccountId { get; private set; }
+        [MaxLength(64)]
         public string BankAccountNumber { get; private set; }
+        [MaxLength(256)]
         public string BankName { get; private set; }
+        [MaxLength(32)]
         public string BankAccountingCode { get; private set; }
         public int CurrencyId { get; private set; }
+        [MaxLength(32)]
         public string CurrencyCode { get; private set; }
         public double CurrencyRate { get; private set; }
         public int SupplierId { get; private set; }
+        [MaxLength(1024)]
         public string SupplierName { get; private set; }
         public bool IsImportSupplier { get; private set; }
+        [MaxLength(256)]
         public string BGCheckNo { get; private set; }
         public double Amount { get; private set; }
         public DateTimeOffset Date { get; private set; }
