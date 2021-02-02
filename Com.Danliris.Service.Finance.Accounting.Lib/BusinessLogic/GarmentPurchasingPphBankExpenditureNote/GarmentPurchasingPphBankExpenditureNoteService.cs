749ba696a9fd76baf2f19b640fc206aab731fdca
ryan <ryan.santosa@moonlay.com>
2021-02-02 08:26:41 +0700

    remove dummy parameter

--- Com.Danliris.Service.Finance.Accounting.Lib/BusinessLogic/GarmentPurchasingPphBankExpenditureNote/GarmentPurchasingPphBankExpenditureNoteService.cs
+++ Com.Danliris.Service.Finance.Accounting.Lib/BusinessLogic/GarmentPurchasingPphBankExpenditureNote/GarmentPurchasingPphBankExpenditureNoteService.cs
@@ -39,8 +39,8 @@ namespace Com.Danliris.Service.Finance.Accounting.Lib.BusinessLogic.GarmentPurch
         {
             var typeDocument = "K";
             var username = _identityService.Username;
-            //model.PphBankInvoiceNo = await _serviceDailyBankTransaction.GetDocumentNo("K", model.Bank.BankCode, username);
-            model.PphBankInvoiceNo = "test";
+            model.PphBankInvoiceNo = await _serviceDailyBankTransaction.GetDocumentNo("K", model.Bank.BankCode, username);
+            //model.PphBankInvoiceNo = "test";
 
             var mapper = _mapper.Map<GarmentPurchasingPphBankExpenditureNoteModel>(model);
 
