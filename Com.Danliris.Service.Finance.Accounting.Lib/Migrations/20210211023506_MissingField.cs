22382af60230f3741d6a574a8a8c917a0ea81bf9
Leslie Aula <leslie.aula@moonlay.com>
2021-02-11 09:35:32 +0700

    Add Migration

---
+++ Com.Danliris.Service.Finance.Accounting.Lib/Migrations/20210211023506_MissingField.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Com.Danliris.Service.Finance.Accounting.Lib.Migrations
+{
+    public partial class MissingField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BillsNo",
+                table: "DPPVATBankExpenditureNoteDetails",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DeliveryOrdersNo",
+                table: "DPPVATBankExpenditureNoteDetails",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentBills",
+                table: "DPPVATBankExpenditureNoteDetails",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BillsNo",
+                table: "DPPVATBankExpenditureNoteDetails");
+
+            migrationBuilder.DropColumn(
+                name: "DeliveryOrdersNo",
+                table: "DPPVATBankExpenditureNoteDetails");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentBills",
+                table: "DPPVATBankExpenditureNoteDetails");
+        }
+    }
+}
