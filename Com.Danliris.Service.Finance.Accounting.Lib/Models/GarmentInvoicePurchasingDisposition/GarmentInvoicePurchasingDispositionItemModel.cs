35bd10e907b3c02f2136b2068d78e00488da2488
ryan <ryan.santosa@moonlay.com>
2021-03-25 11:11:34 +0700

    adding model garmentinvoicePurchaseDisposition

---
+++ Com.Danliris.Service.Finance.Accounting.Lib/Models/GarmentInvoicePurchasingDisposition/GarmentInvoicePurchasingDispositionItemModel.cs
@@ -0,0 +1,32 @@
+﻿using Com.Moonlay.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Com.Danliris.Service.Finance.Accounting.Lib.Models.GarmentInvoicePurchasingDisposition
+{
+    public class GarmentInvoicePurchasingDispositionItemModel:StandardEntity
+    {
+        public int DispositionId { get; private set; }
+        public string DispositionNo { get; private set; }
+        public DateTimeOffset DispositionDate { get; private set; }
+        public DateTimeOffset DipositionDueDate { get; private set; }
+        public string ProformaNo { get; private set; }
+        public string SupplierName { get; private set; }
+        public string Category { get; private set; }
+        public double VATAmount { get; private set; }
+        public double TotalAmount { get; private set; }
+        public double TotalPaid { get; set; }
+        /// <summary>
+        /// Sum of Total Paid With same Disposistion ID
+        /// </summary>
+        public double TotalPaidBefore { get; set; }
+        public int CurrencyId { get; set; }
+        public string CurrencyCode { get; set; }
+        public double CurrencyRate { get; set; }
+        public int GarmentInvoicePurchasingDisposistionId { get; set; }
+        [ForeignKey("GarmentInvoicePurchasingDisposistionId")]
+        public virtual GarmentInvoicePurchasingDispositionModel GarmentInvoicePurchasingDisposition { get; set; }
+    }
+}
---
+++ Com.Danliris.Service.Finance.Accounting.Lib/Models/GarmentInvoicePurchasingDisposition/GarmentInvoicePurchasingDispositionModel.cs
@@ -0,0 +1,29 @@
+﻿using Com.Moonlay.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Danliris.Service.Finance.Accounting.Lib.Models.GarmentInvoicePurchasingDisposition
+{
+    public class GarmentInvoicePurchasingDispositionModel: StandardEntity
+    {
+        public string InvoiceNo { get; private set; }
+        public DateTimeOffset InvoiceDate { get; private set; }
+        public int BankId { get; private set; }
+        public string BankName { get; private set; }
+        public string BankCode { get; private set; }
+        public int BankAccount { get; private set; }
+        public int CurrencyId { get; private set; }
+        public string CurrencyCode { get; private set; }
+        public double CurrencyRate { get; private set; }
+        public DateTimeOffset CurrencyDate { get; private set; }
+        public int SupplierId { get; private set; }
+        public string SupplierName { get; private set; }
+        public string SupplierCode { get; private set; }
+        public bool IsImportSupplier { get; private set; }
+        public string ChequeNo { get; private set; }
+        public string PaymentType { get; private set; }
+        public virtual List<GarmentInvoicePurchasingDispositionItemModel> Items { get; set; }
+
+    }
+}
