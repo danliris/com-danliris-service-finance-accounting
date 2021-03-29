e74d37d90cec63bbe76babf07afb6da6307e6aa1
ryan <ryan.santosa@moonlay.com>
2021-03-29 20:27:43 +0700

    migration

---
+++ Com.Danliris.Service.Finance.Accounting.Lib/Migrations/20210329132726_addingCategoryFieldInGarmmentDispositionExpedition.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Com.Danliris.Service.Finance.Accounting.Lib.Migrations
+{
+    public partial class addingCategoryFieldInGarmmentDispositionExpedition : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "GarmentDispositionExpeditions",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "GarmentDispositionExpeditions");
+        }
+    }
+}
