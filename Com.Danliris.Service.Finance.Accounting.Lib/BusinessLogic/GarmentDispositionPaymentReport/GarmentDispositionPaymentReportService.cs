62bfb8f0eec0e460820164ca1df9157ad8032c07
Leslie Aula <leslie.aula@moonlay.com>
2021-03-29 15:34:58 +0700

    Enhance Disposition Payment Report

---
+++ Com.Danliris.Service.Finance.Accounting.Lib/BusinessLogic/GarmentDispositionPaymentReport/GarmentDispositionPaymentReportService.cs
@@ -0,0 +1,30 @@
+﻿using Com.Danliris.Service.Finance.Accounting.Lib.Enums.Expedition;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Com.Danliris.Service.Finance.Accounting.Lib.BusinessLogic.GarmentDispositionPaymentReport
+{
+    public class GarmentDispositionPaymentReportService : IGarmentDispositionPaymentReportService
+    {
+        public List<PositionOption> GetPositionOptions()
+        {
+            var result = new List<PositionOption>();
+
+            var positions = Enum.GetValues(typeof(GarmentPurchasingExpeditionPosition)).Cast<GarmentPurchasingExpeditionPosition>();
+
+            foreach (var position in positions)
+            {
+                result.Add(new PositionOption(position));
+            }
+
+            return result;
+        }
+
+        public List<GarmentDispositionPaymentReportDto> GetReport(int dispositionId, int supplierId, GarmentPurchasingExpeditionPosition position, string purchasingStaff, DateTimeOffset startDate, DateTimeOffset endDate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Com.Danliris.Service.Finance.Accounting.Lib/BusinessLogic/GarmentDispositionPaymentReport/IGarmentDispositionPaymentReportService.cs
@@ -0,0 +1,13 @@
+﻿using Com.Danliris.Service.Finance.Accounting.Lib.Enums.Expedition;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Danliris.Service.Finance.Accounting.Lib.BusinessLogic.GarmentDispositionPaymentReport
+{
+    public interface IGarmentDispositionPaymentReportService
+    {
+        List<GarmentDispositionPaymentReportDto> GetReport(int dispositionId, int supplierId, GarmentPurchasingExpeditionPosition position, string purchasingStaff, DateTimeOffset startDate, DateTimeOffset endDate);
+        List<PositionOption> GetPositionOptions();
+    }
+}
