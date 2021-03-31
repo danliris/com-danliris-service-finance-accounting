0653e07f9cef499b124e7cd624bee0b6eab4e8df
wilsonmoonlay <54935452+wilsonmoonlay@users.noreply.github.com>
2021-03-31 16:37:28 +0700

    Minor Bug Fix for Validation

--- Com.Danliris.Service.Finance.Accounting.Lib/BusinessLogic/Services/AccountingBook/AccountingBookService.cs
+++ Com.Danliris.Service.Finance.Accounting.Lib/BusinessLogic/Services/AccountingBook/AccountingBookService.cs
@@ -153,7 +153,7 @@ namespace Com.Danliris.Service.Finance.Accounting.Lib.BusinessLogic.Services.Acc
             try
             {
                 var result = await _dbContext.AccountingBooks
-                    .Where(x => id > 0 ? x.Code.Contains(code) && !x.Id.Equals(id) && x.IsDeleted == false : x.Code.Contains(code)).FirstOrDefaultAsync();
+                    .Where(x => id > 0 ? x.Code.Equals(code) && !x.Id.Equals(id) && x.IsDeleted == false : x.Code.Equals(code)).FirstOrDefaultAsync();
 
                 return result;
             }
@@ -198,7 +198,7 @@ namespace Com.Danliris.Service.Finance.Accounting.Lib.BusinessLogic.Services.Acc
             try
             {
                 var result = await _dbContext.AccountingBooks
-                    .Where(x => id > 0 ? x.AccountingBookType.Contains(name) && !x.Id.Equals(id) && x.IsDeleted == false : x.AccountingBookType.Contains(name)).FirstOrDefaultAsync();
+                    .Where(x => id > 0 ? x.AccountingBookType.Equals(name) && !x.Id.Equals(id) && x.IsDeleted == false : x.AccountingBookType.Equals(name)).FirstOrDefaultAsync();
 
                 return result;
             }
