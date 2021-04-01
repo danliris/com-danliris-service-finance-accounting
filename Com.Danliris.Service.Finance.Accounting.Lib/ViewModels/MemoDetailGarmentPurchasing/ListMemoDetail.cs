866d4dfd4c937f3dc9d2cf5d56b188ca08c882b8
Fadhlinp <fadhli.nopri.pratama@gmail.com>
2021-04-01 13:23:14 +0700

    Create Api Get List Memo Detail & Update Api Get Rincian Memo Detail

---
+++ Com.Danliris.Service.Finance.Accounting.Lib/ViewModels/MemoDetailGarmentPurchasing/ListMemoDetail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Com.Danliris.Service.Finance.Accounting.Lib.ViewModels.MemoDetailGarmentPurchasing
+{
+    public class ListMemoDetail
+    {
+        public int Id { get; set; }
+        public int MemoId { get; set; }
+        public string MemoNo { get; set; }
+        public DateTimeOffset MemoDate { get; set; }
+        public int AccountingBookId { get; set; }
+        public string AccountingBookType { get; set; }
+        public int GarmentCurrenciesId { get; set; }
+        public string GarmentCurrenciesCode { get; set; }
+        public int GarmentCurrenciesRate { get; set; }
+        public string Remarks { get; set; }
+        public bool IsPosted { get; set; }
+    }
+}
