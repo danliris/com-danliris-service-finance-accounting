4c01e6255a032e5ebf4a0b1bf9725a627f1f2ca6
Fadhlinp <fadhli.nopri.pratama@gmail.com>
2021-04-12 10:44:05 +0700

    Perubahan cetakan Excel & PDF

--- Com.Danliris.Service.Finance.Accounting.Lib/BusinessLogic/Services/MemoDetailGarmentPurchasing/ExcelGenerator/MemoDetailGarmentExcelGenerator.cs
+++ Com.Danliris.Service.Finance.Accounting.Lib/BusinessLogic/Services/MemoDetailGarmentPurchasing/ExcelGenerator/MemoDetailGarmentExcelGenerator.cs
@@ -71,22 +71,25 @@ namespace Com.Danliris.Service.Finance.Accounting.Lib.BusinessLogic.Services.Mem
             worksheet.Cells["E4"].Style.Font.Size = 14;
             worksheet.Cells["F4"].Value = "No. Surat Jalan";
             worksheet.Cells["F4"].Style.Font.Size = 14;
-            worksheet.Cells["G4"].Value = "Keterangan";
+            worksheet.Cells["G4"].Value = "Kode Supplier";
             worksheet.Cells["G4"].Style.Font.Size = 14;
-            worksheet.Cells["H4"].Value = "Kurs";
+            worksheet.Cells["H4"].Value = "Nama Supplier";
             worksheet.Cells["H4"].Style.Font.Size = 14;
-            worksheet.Cells["I4"].Value = "Kurs Pembayaran";
+            worksheet.Cells["I4"].Value = "Kurs";
             worksheet.Cells["I4"].Style.Font.Size = 14;
-            worksheet.Cells["J4"].Value = "Kurs Beli";
+            worksheet.Cells["J4"].Value = "Kurs Pembayaran";
             worksheet.Cells["J4"].Style.Font.Size = 14;
-            worksheet.Cells["K4"].Value = "Jumlah";
+            worksheet.Cells["K4"].Value = "Kurs Beli";
             worksheet.Cells["K4"].Style.Font.Size = 14;
-            worksheet.Cells["L4"].Value = "Jumlah Dalam Rupiah";
+            worksheet.Cells["L4"].Value = "Jumlah";
             worksheet.Cells["L4"].Style.Font.Size = 14;
+            worksheet.Cells["M4"].Value = "Jumlah Dalam Rupiah";
+            worksheet.Cells["M4"].Style.Font.Size = 14;
 
             if (data.Data.Count > 0)
             {
                 var currentRow = 5;
+                var total = 0;
                 
                 foreach (var item in data.Data)
                 {
@@ -106,37 +109,74 @@ namespace Com.Danliris.Service.Finance.Accounting.Lib.BusinessLogic.Services.Mem
                     worksheet.Cells[$"E{currentRow}"].Style.Font.Size = 14;
                     worksheet.Cells[$"F{currentRow}"].Value = item.GarmentDeliveryOrderNo;
                     worksheet.Cells[$"F{currentRow}"].Style.Font.Size = 14;
-                    worksheet.Cells[$"G{currentRow}"].Value = item.RemarksDetail;
+                    worksheet.Cells[$"G{currentRow}"].Value = GetSupplierCode(item.RemarksDetail);
                     worksheet.Cells[$"G{currentRow}"].Style.Font.Size = 14;
-                    worksheet.Cells[$"H{currentRow}"].Value = item.CurrencyCode;
+                    worksheet.Cells[$"H{currentRow}"].Value = GetSupplierName(item.RemarksDetail);
                     worksheet.Cells[$"H{currentRow}"].Style.Font.Size = 14;
-                    worksheet.Cells[$"I{currentRow}"].Value = item.PaymentRate;
+                    worksheet.Cells[$"I{currentRow}"].Value = item.CurrencyCode;
                     worksheet.Cells[$"I{currentRow}"].Style.Font.Size = 14;
-                    worksheet.Cells[$"I{currentRow}"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Right;
-                    worksheet.Cells[$"J{currentRow}"].Value = item.PurchasingRate;
+                    worksheet.Cells[$"J{currentRow}"].Value = item.PaymentRate;
                     worksheet.Cells[$"J{currentRow}"].Style.Font.Size = 14;
                     worksheet.Cells[$"J{currentRow}"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Right;
-                    worksheet.Cells[$"K{currentRow}"].Value = item.MemoAmount.ToString("N2");
+                    worksheet.Cells[$"K{currentRow}"].Value = item.PurchasingRate;
                     worksheet.Cells[$"K{currentRow}"].Style.Font.Size = 14;
-                    //worksheet.Cells[$"K{currentRow}"].Style.Numberformat.Format = "#,##0.00";
                     worksheet.Cells[$"K{currentRow}"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Right;
-                    worksheet.Cells[$"L{currentRow}"].Value = item.MemoIdrAmount.ToString("N2");
+                    worksheet.Cells[$"L{currentRow}"].Value = item.MemoAmount.ToString("N2");
                     worksheet.Cells[$"L{currentRow}"].Style.Font.Size = 14;
-                    //worksheet.Cells[$"L{currentRow}"].Style.Numberformat.Format = "#,##0.00";
                     worksheet.Cells[$"L{currentRow}"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Right;
+                    worksheet.Cells[$"M{currentRow}"].Value = item.MemoIdrAmount.ToString("N2");
+                    worksheet.Cells[$"M{currentRow}"].Style.Font.Size = 14;
+                    worksheet.Cells[$"M{currentRow}"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Right;
 
+                    total += item.MemoIdrAmount;
                     currentRow++;
                 }
 
-                worksheet.Cells[$"A4:L{currentRow - 1}"].AutoFitColumns();
-                worksheet.Cells[$"A4:L{currentRow - 1}"].Style.Border.Top.Style = ExcelBorderStyle.Thin;
-                worksheet.Cells[$"A4:L{currentRow - 1}"].Style.Border.Right.Style = ExcelBorderStyle.Thin;
-                worksheet.Cells[$"A4:L{currentRow - 1}"].Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
-                worksheet.Cells[$"A4:L{currentRow - 1}"].Style.Border.Left.Style = ExcelBorderStyle.Thin;
+                worksheet.Cells[$"A4:M{currentRow - 1}"].AutoFitColumns();
+                worksheet.Cells[$"A4:M{currentRow - 1}"].Style.Border.Top.Style = ExcelBorderStyle.Thin;
+                worksheet.Cells[$"A4:M{currentRow - 1}"].Style.Border.Right.Style = ExcelBorderStyle.Thin;
+                worksheet.Cells[$"A4:M{currentRow - 1}"].Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
+                worksheet.Cells[$"A4:M{currentRow - 1}"].Style.Border.Left.Style = ExcelBorderStyle.Thin;
+
+                worksheet.Cells[$"L{currentRow}"].Value = "Total";
+                worksheet.Cells[$"L{currentRow}"].Style.Font.Size = 14;
+                worksheet.Cells[$"L{currentRow}"].Style.Font.Bold = true;
+                worksheet.Cells[$"L{currentRow}"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+
+                worksheet.Cells[$"M{currentRow}"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Right;
+                worksheet.Cells[$"M{currentRow}"].Value = total.ToString("N2");
+                worksheet.Cells[$"M{currentRow}"].Style.Font.Size = 14;
+                worksheet.Cells[$"M{currentRow}"].Style.Font.Bold = true;
+
+                worksheet.Cells[$"L{currentRow}:M{currentRow }"].Style.Border.Top.Style = ExcelBorderStyle.Thin;
+                worksheet.Cells[$"L{currentRow}:M{currentRow }"].Style.Border.Right.Style = ExcelBorderStyle.Thin;
+                worksheet.Cells[$"L{currentRow}:M{currentRow }"].Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
+                worksheet.Cells[$"L{currentRow}:M{currentRow }"].Style.Border.Left.Style = ExcelBorderStyle.Thin;
             }
-            else
+        }
+
+        private static string GetSupplierCode(string remarksDetail)
+        {
+
+            if (remarksDetail.Contains("-"))
             {
+                string[] arrStr = remarksDetail.Split("-");
+                return arrStr[0];
             }
+
+            return remarksDetail;
+
+        }
+
+        private static string GetSupplierName(string remarksDetail)
+        {
+            if (remarksDetail.Contains("-"))
+            {
+                string[] arrStr = remarksDetail.Split("-");
+                return arrStr[1];
+            }
+
+            return remarksDetail;
         }
     }
 
