32451bd17051c41fa0ae1547ba5576f34abae8f0
ryan <ryan.santosa@moonlay.com>
2021-04-15 10:34:41 +0700

    Enhance Query local Credit balance

---
+++ Com.Danliris.Service.Finance.Accounting.Lib/ViewModels/CreditBalance/CreditBalanceAccountViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Com.Danliris.Service.Finance.Accounting.Lib.Models.CreditorAccount;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Danliris.Service.Finance.Accounting.Lib.ViewModels.CreditBalance
+{
+    public class CreditBalanceAccountViewModel
+    {
+        //public decimal StartBalance { get; set; }
+        public string SupplierCode { get; set; }
+        public string DivisionCode { get; set; }
+        public string CurrencyCode { get; set; }
+        public DateTimeOffset? UnitReceiptNoteDate { get; set; }
+        public string Products { get; set; }
+        public decimal UnitReceiptMutation { get; set; }
+        public decimal BankExpenditureNoteMutation { get; set; }
+        public decimal FinalBalance { get; set; }
+        public string SupplierName { get; set; }
+        public decimal CurrencyRate { get; set; }
+        public string DivisionName { get; set; }
+        public CreditBalanceAccountViewModel(CreditorAccountModel creditorAccountModel)
+        {
+            SupplierCode = creditorAccountModel.SupplierCode;
+            DivisionCode = creditorAccountModel.DivisionCode;
+            CurrencyCode = creditorAccountModel.CurrencyCode;
+            UnitReceiptNoteDate = creditorAccountModel.UnitReceiptNoteDate;
+            Products = creditorAccountModel.Products;
+            UnitReceiptMutation = creditorAccountModel.UnitReceiptMutation;
+            BankExpenditureNoteMutation = creditorAccountModel.BankExpenditureNoteMutation;
+            SupplierName = creditorAccountModel.SupplierName;
+            CurrencyRate = creditorAccountModel.CurrencyRate;
+            DivisionName = creditorAccountModel.DivisionName;
+            FinalBalance = creditorAccountModel.FinalBalance;
+        }
+    }
+}
