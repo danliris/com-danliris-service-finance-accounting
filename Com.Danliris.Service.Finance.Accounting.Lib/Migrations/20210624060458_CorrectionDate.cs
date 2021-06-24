3738a310d108dd783052f5bd74f880d24d944a2d
Leslie Aula <leslie.aula@moonlay.com>
2021-06-24 14:05:16 +0800

    fix creditor account

---
+++ Com.Danliris.Service.Finance.Accounting.Lib/Migrations/20210624060458_CorrectionDate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Com.Danliris.Service.Finance.Accounting.Lib.Migrations
+{
+    public partial class CorrectionDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "UnitPaymentCorrectionDate",
+                table: "CreditorAccounts",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UnitPaymentCorrectionDate",
+                table: "CreditorAccounts");
+        }
+    }
+}
