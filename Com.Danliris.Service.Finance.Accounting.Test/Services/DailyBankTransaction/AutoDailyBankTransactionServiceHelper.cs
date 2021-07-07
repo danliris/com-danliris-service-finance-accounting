4985b246b2623937e975c19c4914ab29fcb20bc1
ryan <ryan.santosa@moonlay.com>
2021-07-07 11:14:08 +0700

    enhance unit test

--- Com.Danliris.Service.Finance.Accounting.Test/Services/DailyBankTransaction/AutoDailyBankTransactionServiceHelper.cs
+++ Com.Danliris.Service.Finance.Accounting.Test/Services/DailyBankTransaction/AutoDailyBankTransactionServiceHelper.cs
@@ -4,6 +4,7 @@ using Com.Danliris.Service.Finance.Accounting.Lib.BusinessLogic.VBRequestDocumen
 using Com.Danliris.Service.Finance.Accounting.Lib.Models.GarmentInvoicePurchasingDisposition;
 using Com.Danliris.Service.Finance.Accounting.Lib.Models.OthersExpenditureProofDocument;
 using Com.Danliris.Service.Finance.Accounting.Lib.Models.PaymentDispositionNote;
+using Com.Danliris.Service.Finance.Accounting.Lib.ViewModels.NewIntegrationViewModel;
 using Com.Danliris.Service.Finance.Accounting.Lib.ViewModels.OthersExpenditureProofDocumentViewModels;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -12,6 +13,12 @@ namespace Com.Danliris.Service.Finance.Accounting.Test.Services.DailyBankTransac
 {
     public class AutoDailyBankTransactionServiceHelper : IAutoDailyBankTransactionService
     {
+        public async Task<int> AutoCreateFromClearenceVB(List<int> vbRealizationIds, AccountBankViewModel bank)
+        {
+            await Task.Delay(1000);
+            return await Task.FromResult(1);
+        }
+
         public async Task<int> AutoCreateFromGarmentInvoicePurchasingDisposition(GarmentInvoicePurchasingDispositionModel model)
         {
             await Task.Delay(1000);
