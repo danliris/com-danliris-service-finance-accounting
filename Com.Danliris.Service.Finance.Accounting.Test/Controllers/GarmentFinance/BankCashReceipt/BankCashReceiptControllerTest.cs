0dd0b24d7126eafa52661cd6200ed5c27eece2b9
Helmi Adi Prasetyo <helmi.prasetyo12@gmail.com>
2021-07-21 15:44:36 +0700

    Update BankCashReceiptControllerTest.cs

--- Com.Danliris.Service.Finance.Accounting.Test/Controllers/GarmentFinance/BankCashReceipt/BankCashReceiptControllerTest.cs
+++ Com.Danliris.Service.Finance.Accounting.Test/Controllers/GarmentFinance/BankCashReceipt/BankCashReceiptControllerTest.cs
@@ -338,16 +338,17 @@ namespace Com.Danliris.Service.Finance.Accounting.Test.Controllers.GarmentFinanc
         {
             //Setup
             var mocks = GetMocks();
-            mocks.Service.Setup(f => f.ReadByIdAsync(It.IsAny<int>())).ReturnsAsync(new BankCashReceiptModel());
+            mocks.Service.Setup(f => f.ReadByIdAsync(It.IsAny<int>())).ReturnsAsync(new BankCashReceiptModel() {ReceiptNo = "123" });
             BankCashReceiptController controller = GetController(mocks);
 
             controller.ControllerContext.HttpContext.Request.Headers["Accept"] = "application/pdf";
             controller.ControllerContext.HttpContext.Request.Headers["x-timezone-offset"] = "7";
 
-            IActionResult response = await controller.GetById(1);
+            var response = await controller.GetById(1);
 
             //Assert
             Assert.Equal("application/pdf", response.GetType().GetProperty("ContentType").GetValue(response, null));
+
 
         }
 
