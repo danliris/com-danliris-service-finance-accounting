4aff17db8ea63f9b8c230c662d24e618136ebdef
Helmi Adi Prasetyo <helmi.prasetyo12@gmail.com>
2021-07-21 15:07:05 +0700

    Update GarmentFinanceBankCashReceiptPdfTemplateTest.cs

--- Com.Danliris.Service.Finance.Accounting.Test/PDFTemplates/GarmentFinanceBankCashReceiptPdfTemplateTest.cs
+++ Com.Danliris.Service.Finance.Accounting.Test/PDFTemplates/GarmentFinanceBankCashReceiptPdfTemplateTest.cs
@@ -46,7 +46,96 @@ namespace Com.Danliris.Service.Finance.Accounting.Test.PDFTemplates
                         Code = "Code",
                         Currency = new Lib.ViewModels.NewIntegrationViewModel.CurrencyViewModel()
                         {
-                            Code = "SGD",
+                            Code = "IDR",
+                            Description = "Description",
+                            Id = 1,
+                            Rate = 1,
+                            Symbol = "Rp"
+                        },
+                        Id = 1
+                    },
+                    Id = 1,
+                    Items = new List<BankCashReceiptItemViewModel>()
+                    {
+                        new BankCashReceiptItemViewModel()
+                        {
+                            Id = 1,
+                            AccAmount = new Lib.ViewModels.NewIntegrationViewModel.ChartOfAccountViewModel()
+                            {
+                                Id = "1",
+                                Code = "code",
+                                Name = "name",
+                            },
+                            AccNumber = new Lib.ViewModels.NewIntegrationViewModel.ChartOfAccountViewModel()
+                            {
+                                Id = "1",
+                                Code = "code",
+                                Name = "name",
+                            },
+                            AccSub = new Lib.ViewModels.NewIntegrationViewModel.ChartOfAccountViewModel()
+                            {
+                                Id = "1",
+                                Code = "code",
+                                Name = "name",
+                            },
+                            AccUnit = new Lib.ViewModels.NewIntegrationViewModel.ChartOfAccountViewModel()
+                            {
+                                Id = "1",
+                                Code = "code",
+                                Name = "name",
+                            },
+                            Amount = 1,
+                            C1A = 1,
+                            C1B = 1,
+                            C2A = 1,
+                            C2B = 1,
+                            C2C = 1,
+                            NoteNumber = "noteNumber",
+                            Remarks = "remarks",
+                            Summary = 1,
+                        }
+                    }
+                };
+            }
+        }
+
+        public BankCashReceiptViewModel viewModelDollar
+        {
+            get
+            {
+                return new BankCashReceiptViewModel()
+                {
+                    ReceiptNo = "receiptNo",
+                    Amount = 0,
+                    Currency = new Lib.ViewModels.NewIntegrationViewModel.CurrencyViewModel()
+                    {
+                        Id = 1,
+                        Code = "code",
+                        Description = "description",
+                        Rate = 1,
+                        Symbol = "symbol",
+                    },
+                    NumberingCode = "numberingCode",
+                    ReceiptDate = DateTimeOffset.Now,
+                    Remarks = "remarks",
+                    DebitCoa = new Lib.ViewModels.NewIntegrationViewModel.ChartOfAccountViewModel()
+                    {
+                        Id = "1",
+                        Code = "code",
+                        Name = "name",
+                    },
+                    IncomeType = "incomeType",
+                    Bank = new Lib.ViewModels.NewIntegrationViewModel.AccountBankViewModel()
+                    {
+                        AccountCOA = "AccountCOA",
+                        AccountName = "AccountName",
+                        AccountNumber = "AccountNumber",
+                        BankCode = "BankCode",
+                        BankName = "BankName",
+                        Code = "Code",
+                        Currency = new Lib.ViewModels.NewIntegrationViewModel.CurrencyViewModel()
+                        {
+                            Code = "USD",
                             Description = "Description",
                             Id = 1,
                             Rate = 1,
@@ -104,6 +193,14 @@ namespace Com.Danliris.Service.Finance.Accounting.Test.PDFTemplates
         {
             GarmentFinanceBankCashReceiptPdfTemplate PdfTemplate = new GarmentFinanceBankCashReceiptPdfTemplate();
             MemoryStream result = PdfTemplate.GeneratePdfTemplate(viewModelIDR, 7);
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void shouldSuccessPDFTemplateWithCurrencyUSD()
+        {
+            GarmentFinanceBankCashReceiptPdfTemplate PdfTemplate = new GarmentFinanceBankCashReceiptPdfTemplate();
+            MemoryStream result = PdfTemplate.GeneratePdfTemplate(viewModelDollar, 7);
             Assert.NotNull(result);
         }
     }
