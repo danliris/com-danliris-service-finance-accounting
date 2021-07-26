fd79257c8d7cca45e6d4255bb6de5b4e988be991
achensetyawati <setyawati@moonlay.com>
2021-07-26 08:21:28 +0700

    Garment Finance Memorial

--- Com.Danliris.Service.Finance.Accounting.Lib/FinanceDbContext.cs
+++ Com.Danliris.Service.Finance.Accounting.Lib/FinanceDbContext.cs
@@ -27,6 +27,7 @@ using Com.Danliris.Service.Finance.Accounting.Lib.Models.GarmentDebtBalance;
 using Com.Danliris.Service.Finance.Accounting.Lib.Models.GarmentDispositionExpedition;
 using Com.Danliris.Service.Finance.Accounting.Lib.Models.MemoGarmentPurchasing;
 using Com.Danliris.Service.Finance.Accounting.Lib.Models.AccountingBook;
+using Com.Danliris.Service.Finance.Accounting.Lib.Models.GarmentFinance.Memorial;
 
 namespace Com.Danliris.Service.Finance.Accounting.Lib
 {
@@ -109,7 +110,7 @@ namespace Com.Danliris.Service.Finance.Accounting.Lib
         public DbSet<GarmentInvoicePurchasingDispositionModel> GarmentInvoicePurchasingDispositions { get; set; }
         public DbSet<GarmentInvoicePurchasingDispositionItemModel> GarmentInvoicePurchasingDispositionItems { get; set; }
         public DbSet<GarmentDebtBalanceModel> GarmentDebtBalances { get; set; }
-
+        
         public DbSet<MemoGarmentPurchasingModel> MemoGarmentPurchasings { get; set; }
         public DbSet<MemoGarmentPurchasingDetailModel> MemoGarmentPurchasingDetails { get; set; }
         public DbSet<MemoDetailGarmentPurchasingModel> MemoDetailGarmentPurchasings { get; set; }
@@ -117,6 +118,9 @@ namespace Com.Danliris.Service.Finance.Accounting.Lib
         public DbSet<MemoDetailGarmentPurchasingDetailModel> MemoDetailGarmentPurchasingDetails { get; set; }
 
         public DbSet<AccountingBookModel> AccountingBooks { get; set; }
+
+        public DbSet<GarmentFinanceMemorialModel> GarmentFinanceMemorials { get; set; }
+        public DbSet<GarmentFinanceMemorialItemModel> GarmentFinanceMemorialItems { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
---
+++ Com.Danliris.Service.Finance.Accounting.Lib/Models/GarmentFinance/Memorial/GarmentFinanceMemorialItemModel.cs
@@ -0,0 +1,23 @@
+﻿using Com.Moonlay.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Com.Danliris.Service.Finance.Accounting.Lib.Models.GarmentFinance.Memorial
+{
+    public class GarmentFinanceMemorialItemModel : StandardEntity
+    {
+        public virtual int MemorialId { get; set; }
+        [ForeignKey("MemorialId")]
+        public virtual GarmentFinanceMemorialModel GarmentFinanceMemorialModel { get; set; }
+        public int COAId { get; set; }
+        [MaxLength(20)]
+        public string COACode { get; set; }
+        [MaxLength(100)]
+        public string COAName { get; set; }
+        public double Debit { get; set; }
+        public double Credit { get; set; }
+    }
+}
