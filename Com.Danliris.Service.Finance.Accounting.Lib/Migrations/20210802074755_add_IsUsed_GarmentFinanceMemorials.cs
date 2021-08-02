3bbb6aedc94abdc1773942d51d01d1ef5805a087
achensetyawati <setyawati@moonlay.com>
2021-08-02 15:26:18 +0700

    update memorial table

---
+++ Com.Danliris.Service.Finance.Accounting.Lib/Migrations/20210802074755_add_IsUsed_GarmentFinanceMemorials.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Com.Danliris.Service.Finance.Accounting.Lib.Migrations
+{
+    public partial class add_IsUsed_GarmentFinanceMemorials : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsUsed",
+                table: "GarmentFinanceMemorials",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsUsed",
+                table: "GarmentFinanceMemorials");
+        }
+    }
+}
