32bc8206598a9e2b148653e5bbbc9676f70566fc
Leslie Aula <leslie.aula@moonlay.com>
2021-08-19 05:52:55 +0800

    Fix Get Detail

---
+++ Com.Danliris.Service.Finance.Accounting.Lib/BusinessLogic/Interfaces/CreditBalance/CreditBalanceDetailViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Com.Danliris.Service.Finance.Accounting.Lib.ViewModels.CreditBalance;
+using System;
+
+namespace Com.Danliris.Service.Finance.Accounting.Lib.BusinessLogic.Interfaces.CreditBalance
+{
+    public class CreditBalanceDetailViewModel : CreditBalanceViewModel
+    {
+        public string ExternalPurchaseOrderNo { get; set; }
+        public string IncomeTaxNo { get; set; }
+        public decimal DPPAmount { get; set; }
+        public decimal VATAmount { get; set; }
+        public decimal IncomeTaxAmount { get; set; }
+        public DateTimeOffset? Date { get; set; }
+    }
+}
