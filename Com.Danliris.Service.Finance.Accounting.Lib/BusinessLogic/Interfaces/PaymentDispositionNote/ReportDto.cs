d55777fc8cccd08a9132da980e5e810079a1db3e
Leslie Aula <leslie.aula@moonlay.com>
2021-09-03 17:49:12 +0800

    Fix Calculation

--- Com.Danliris.Service.Finance.Accounting.Lib/BusinessLogic/Interfaces/PaymentDispositionNote/ReportDto.cs
+++ Com.Danliris.Service.Finance.Accounting.Lib/BusinessLogic/Interfaces/PaymentDispositionNote/ReportDto.cs
@@ -27,7 +27,7 @@ namespace Com.Danliris.Service.Finance.Accounting.Lib.BusinessLogic.Interfaces.P
             DivisionName = divisionName;
             VATAmount = vATAmount;
             if (bankCurrencyCode != currencyCode)
-                PaidAmount = Math.Round(paidAmount, 2) * (currencyRate == 0 ? 1 : currencyRate);
+                PaidAmount = paidAmount * (currencyRate == 0 ? 1 : currencyRate);
             else
                 PaidAmount = paidAmount;
 
