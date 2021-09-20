bdb4ac2857d12935d7720267ecbb43939ec61a56
achensetyawati <setyawati@moonlay.com>
2021-09-20 15:13:59 +0700

    garmentLocalDebitur Balance

---
+++ Com.Danliris.Service.Finance.Accounting.Lib/BusinessLogic/GarmentFinance/LocalDebiturBalance/IGarmentLocalDebiturBalanceService.cs
@@ -0,0 +1,16 @@
+﻿using Com.Danliris.Service.Finance.Accounting.Lib.Models.GarmentFinance.LocalDebiturBalance;
+using Com.Danliris.Service.Finance.Accounting.Lib.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Danliris.Service.Finance.Accounting.Lib.BusinessLogic.GarmentFinance.LocalDebiturBalance
+{
+    public interface IGarmentLocalDebiturBalanceService
+    {
+        ReadResponse<GarmentLocalDebiturBalanceModel> Read(int page, int size, string order, List<string> select, string keyword, string filter = "{}");
+        
+        Task<GarmentLocalDebiturBalanceModel> ReadByIdAsync(int id);
+    }
+}
