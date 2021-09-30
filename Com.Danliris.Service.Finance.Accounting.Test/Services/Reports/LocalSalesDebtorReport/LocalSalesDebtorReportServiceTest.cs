195814e2ada4eff720f818f6bdd99c6dcd38cdaa
Helmi Adi Prasetyo <helmi.prasetyo12@gmail.com>
2021-09-30 09:40:23 +0700

    Update LocalSalesDebtorReportServiceTest.cs

--- Com.Danliris.Service.Finance.Accounting.Test/Services/Reports/LocalSalesDebtorReport/LocalSalesDebtorReportServiceTest.cs
+++ Com.Danliris.Service.Finance.Accounting.Test/Services/Reports/LocalSalesDebtorReport/LocalSalesDebtorReportServiceTest.cs
@@ -496,5 +496,54 @@ namespace Com.Danliris.Service.Finance.Accounting.Test.Services.Reports.LocalSal
             Assert.NotNull(response12);
         }
 
+        [Fact]
+        public async Task Should_Error_Get_Local_Sales_Data()
+        {
+            var serviceProviderMock = GetServiceProvider();
+
+            var dbContext = GetDbContext(GetCurrentAsyncMethod());
+            serviceProviderMock
+                .Setup(serviceProvider => serviceProvider.GetService(typeof(FinanceDbContext)))
+                .Returns(dbContext);
+
+            var serviceProvider1 = new Mock<IServiceProvider>();
+
+            var httpClientService = new Mock<IHttpClientService>();
+            serviceProviderMock
+                .Setup(x => x.GetService(typeof(IIdentityService)))
+                .Returns(new IdentityService() { Token = "Token", Username = "Test", TimezoneOffset = 7 });
+
+            HttpResponseMessage message = new HttpResponseMessage(System.Net.HttpStatusCode.InternalServerError);
+            message.Content = new StringContent("{\"data\":[{\"salesContractNo\":null,\"localSalesContractId\":0,\"noteNo\":\"21/LBJ/00003\",\"date\":\"2021-08-31T17:00:00+00:00\",\"transactionType\":null,\"buyer\":{\"id\":63,\"code\":\"IJA\",\"name\":\"PT INDORENZA JAYA ABADI\",\"address\":null,\"npwp\":null,\"country\":null,\"nik\":null,\"kaberType\":null},\"tempo\":0,\"expenditureNo\":null,\"dispositionNo\":null,\"useVat\":false,\"remark\":null,\"isUsed\":false,\"paymentType\":null,\"amount\":55000000.0,\"items\":null,\"id\":29,\"active\":false,\"createdUtc\":\"0001 - 01 - 01T00: 00:00\",\"createdBy\":null,\"createdAgent\":null,\"lastModifiedUtc\":\"0001 - 01 - 01T00: 00:00\",\"lastModifiedBy\":null,\"lastModifiedAgent\":null,\"isDeleted\":false,\"deletedUtc\":\"0001 - 01 - 01T00: 00:00\",\"deletedBy\":null,\"deletedAgent\":null},]}");
+
+            httpClientService
+                .Setup(x => x.GetAsync(It.Is<string>(s => s.Contains("garment-shipping/local-sales-notes/localSalesDebtor"))))
+                .ReturnsAsync(message);
+
+            serviceProvider1
+                .Setup(x => x.GetService(typeof(IHttpClientService)))
+                .Returns(new HttpClientTestService());
+
+
+            serviceProvider1
+                .Setup(x => x.GetService(typeof(IIdentityService)))
+                .Returns(new IdentityService() { Token = "Token", Username = "Test", TimezoneOffset = 7 });
+
+            serviceProvider1
+                .Setup(x => x.GetService(typeof(IHttpClientService)))
+                .Returns(httpClientService.Object);
+
+            GarmentFinanceMemorialDetailLocalService serviceMemo = new GarmentFinanceMemorialDetailLocalService(GetServiceProvider().Object, _dbContext(GetCurrentMethod()));
+            GarmentFinanceMemorialDetailLocalModel modelMemo = _dataUtilMemo(serviceMemo, GetCurrentMethod()).GetNewData();
+
+            GarmentFinanceBankCashReceiptDetailLocalService serviceBankCash = new GarmentFinanceBankCashReceiptDetailLocalService(serviceProviderMock.Object);
+            GarmentFinanceBankCashReceiptDetailLocalModel cashReceiptDetailModel = await _dataUtilBankCash(serviceBankCash).GetTestData();
+            LocalSalesDebtorReportService service = new LocalSalesDebtorReportService(serviceProvider1.Object, _dbContext(GetCurrentMethod()));
+
+            var response = await service.GetMonitoring(DateTimeOffset.Now.Month, DateTimeOffset.Now.Year, 7);
+
+            Assert.Empty(response);
+        }
+
     }
 }
