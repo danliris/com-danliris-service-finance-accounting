57f92af63623fccb18cfe47b11ad2891159c2223
ichwankm <ichwankm@gmail.com>
2021-12-02 11:40:23 +0700

    fix reference type pengeluaran bank lain-lain

--- Com.Danliris.Service.Finance.Accounting.Lib/BusinessLogic/Services/DailyBankTransaction/AutoDailyBankTransactionService.cs
+++ Com.Danliris.Service.Finance.Accounting.Lib/BusinessLogic/Services/DailyBankTransaction/AutoDailyBankTransactionService.cs
@@ -204,7 +204,7 @@ namespace Com.Danliris.Service.Finance.Accounting.Lib.BusinessLogic.Services.Dai
                 Nominal = itemModels.Sum(item => item.Debit),
                 CurrencyRate = (decimal)model.CurrencyRate,
                 ReferenceNo = model.DocumentNo,
-                ReferenceType = "Pembayaran Lain - lain",
+                ReferenceType = "Pengeluaran Bank Lain-lain",
                 Remark = $"{model.Remark}\n\nPembayaran atas {accountBank.Currency.Code} dengan nominal {string.Format("{0:n}", total)}",
                 SourceType = model.Type,
                 Status = "OUT",
