67c5beebb045f6f8ff36fbb357638ed3d9692655
ichwankm <ichwankm@gmail.com>
2022-01-05 14:10:06 +0700

    Enhance StartBalance in CreditBalance

---
+++ Com.Danliris.Service.Finance.Accounting.Lib/Migrations/20220105060659_Add_IsStartBalance_CreditorAccount.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Com.Danliris.Service.Finance.Accounting.Lib.Migrations
+{
+    public partial class Add_IsStartBalance_CreditorAccount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsStartBalance",
+                table: "CreditorAccounts",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsStartBalance",
+                table: "CreditorAccounts");
+        }
+    }
+}
