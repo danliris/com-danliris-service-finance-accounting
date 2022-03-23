f4bddc799b7519cfabf28b481ff0383ed1db5c7c
supri0343 <supri0343@gmail.com>
2022-03-23 16:09:27 +0700

    enhance_RealizationVBNonPO_ppn

---
+++ Com.Danliris.Service.Finance.Accounting.Lib/ViewModels/VBRealizationDocumentNonPO/VatTaxViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Danliris.Service.Finance.Accounting.Lib.ViewModels.VBRealizationDocumentNonPO
+{
+    public class VatTaxViewModel
+    {
+        public string Id { get; set; }
+        public string Rate { get; set; }
+    }
+}
